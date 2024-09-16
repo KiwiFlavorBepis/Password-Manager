6169f9b2e7b9947aa34273cdceba76a5047f5a1e
Kieran Williams <kww26@case.edu>
2024-09-16 17:08:37 -0400

    bruh

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+
+    private String token = "Token";
+
+    private static void checkFile() {
+        //if file exists
+        // verify key with token        //else
+    }
+    private static boolean verifyToken(String token) {
+
+        return false;
+    }
+
+    private static boolean addPassword(String label, String password, String key) {
+
+        return false;
+    }
+
+    private static String promptUser(String prompt) {
+        System.out.println(prompt);
+        Scanner userInput = new Scanner(System.in);
+        //return scanner input
+        return userInput.nextLine();
+    }
+}
